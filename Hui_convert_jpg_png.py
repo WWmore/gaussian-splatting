d348506a6c570ba90d4e842143a983f9d905f7db
WWmore <i1103552872@gmail.com>
2023-09-26 08:51:54 +0300

    Create Hui_convert_jpg_png.py

---
+++ Hui_convert_jpg_png.py
@@ -0,0 +1,30 @@
+
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
+#print(path) ## c:\Users\WANGH0M\gaussian-splatting
+
+# Get list of HEIF and HEIC files in directory
+#directory = path + '\data_bonsai_old\input'
+
+directory = r'C:\Users\WANGH0M\gaussian-splatting\data_bonsai_old\input'
+directory = r'C:\Users\WANGH0M\gaussian-splatting\data_bonsai_new\input'
+
+files = [f for f in os.listdir(directory) if f.endswith('.HEIC') or f.endswith('.heif')]
+#print(files)
+
+# Convert each file to JPEG
+for filename in files:
+    #image = Image.open(os.path.join(directory, filename))
+
+    register_heif_opener()
+    image = Image.open(directory + '\\' + filename)
+    # print(image)
+
+    half = 0.5
+    out = image.resize( [int(half * s) for s in image.size] )
+
+    out.save(os.path.join(directory, os.path.splitext(filename)[0] + '.png'))
